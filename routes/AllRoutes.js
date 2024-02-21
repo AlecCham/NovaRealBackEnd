9f660a78a77c71580982845a9330abe1522fdbd7
Alec Chaprak <achaprak@gmail.com>
2024-02-21 14:59:59 -0800

    Updated

--- routes/AllRoutes.js
+++ routes/AllRoutes.js
@@ -11,7 +11,7 @@ allroutes.get('/', (req, res) => {
     res.send("Welcome to the Nova Real Estate back end server");
 });
 
-// Fetch all houses - GET request
+// Fetch all houses - GET request chan
 allroutes.get('/houses', async (req, res) => {
     console.log("Reached /houses");
     try {
